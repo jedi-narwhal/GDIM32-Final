a0b75b0467e6ff1a654b018776c82db231e946aa
jedi-narwhal <soniasnlep@yahoo.com>
2026-02-17 18:32:07 -0800

    player movement done

---
+++ GDIM32Final/Assets/Scripts/player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+        private Transform _cameraTrans;
+    private float _rotationX;
+    private float _rotationY;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _mouseSensitivity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+        float mouseY = Input.GetAxis("Mouse Y");
+        _rotationY += mouseY * _mouseSensitivity;
+        _rotationY = Mathf.Clamp(_rotationY, -60.0f, 60.0f);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        _rotationX += mouseX * _mouseSensitivity;
+
+        _cameraTrans.localEulerAngles = new Vector3(-_rotationY, 0, 0);
+        transform.localEulerAngles = new Vector3(0, _rotationX, 0);
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(
+            ((vertical * Vector3.forward) + (horizontal * Vector3.right))
+            * _moveSpeed * Time.deltaTime
+        );
+    }
+}
