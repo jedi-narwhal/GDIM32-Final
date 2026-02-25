0b2cc530c5d2ca6d31c13ad3a4fbe6ee61fff214
Minjoo <minjoos5@uci.edu>
2026-02-24 18:48:06 -0800

    deleted timer text & updated timer code

--- GDIM32Final/Assets/Scripts/Timer.cs
+++ GDIM32Final/Assets/Scripts/Timer.cs
@@ -1,13 +1,21 @@
 using System.Collections;
+using TMPro;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _timerText;
     float time = 20f;
     
     void Update()
     {
-        
+        time -= Time.deltaTime;
+        _timerText.text = time.ToString("F2");
+
+        if (time <= 0)
+        {
+            Debug.Log("Time Over");
+        }
     }
 }
