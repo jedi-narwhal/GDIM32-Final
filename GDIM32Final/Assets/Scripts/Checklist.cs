aa30d008e7e943ef6c8bcd468af790e8d8c5bf0a
minjoos5 <minjoos5@uci.edu>
2026-02-25 17:12:01 -0800

    added inventory script

---
+++ GDIM32Final/Assets/Scripts/Checklist.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checklist : MonoBehaviour
+{
+    private bool _isItemAvailable = false;
+    public Button _inventory;
+    public GameObject _item;
+    
+    void Start()
+    {
+        _isItemAvailable = false;
+    }
+
+
+    //if the player clicks the correct item, then _isItemAvailable = true;
+
+    void _itemChosen()
+    {
+        //if item was clicked, _isItemAvailable = true, and stores the item
+        //nothing is in the inventory or the player didn't click anything useful --> no changes
+    }
+}
