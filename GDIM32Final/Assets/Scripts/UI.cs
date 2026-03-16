c1853a35133c08108a469faefd9745f14bf8467b
Minjoo <minjoos5@uci.edu>
2026-03-16 15:00:12 -0700

    inventory appears after clicking the game start screen

--- GDIM32Final/Assets/Scripts/UI.cs
+++ GDIM32Final/Assets/Scripts/UI.cs
@@ -9,17 +9,27 @@ public class UI : MonoBehaviour
 
     [SerializeField] private GameObject _gameoverMenu;
 
+    [SerializeField] private GameObject _gamewinMenu;
+
+    [SerializeField] private GameObject _inventoryBattery;
+
+    [SerializeField] private GameObject _inventoryKey;
+
     void Awake()
     {
         _startMenu.SetActive(true);
+        _inventoryBattery.SetActive(false);
+        _inventoryKey.SetActive(false);
     }
-
     public void GameStart()
     {
         _startMenu.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         Debug.Log("Game Start");
+
+        _inventoryBattery.SetActive(true);
+        _inventoryKey.SetActive(true);
     }
 
     public void GameOver()
@@ -29,6 +39,15 @@ public class UI : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         Debug.Log("Game Over");
+    }
+
+    public void GameWin()
+    {
+        Time.timeScale = 0f;
+        _gamewinMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Debug.Log("Game Win");
     }
 
     public void Restart()
