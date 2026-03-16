b37414da0d2c47e1afafa66cfee2accee58f6c42
jedi-narwhal <soniasnlep@yahoo.com>
2026-03-15 18:54:17 -0700

    door done

--- GDIM32Final/Assets/Scripts/door.cs
+++ GDIM32Final/Assets/Scripts/door.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class door : MonoBehaviour
+
 {
+     public bool _hasKey = false;
+    [SerializeField] private GameObject _gameoverMenu;
+    [SerializeField] public GameObject _lockedtext;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,4 +24,19 @@ public class door : MonoBehaviour
         //if (player.Instance._hasKey == true && )
     }
   // show win screen/diable timer and player
+
+   void OnMouseUpAsButton()
+    {
+        if  (player.Instance._hasKey)
+        {
+            _gameoverMenu.SetActive(true);
+            player.Instance._ui.GameOver();
+            Time.timeScale = 0f;
+             
+        }
+        else
+        {
+             _lockedtext.SetActive(true);      
+        }
+    }
 }
