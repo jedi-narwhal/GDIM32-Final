a17bb25600552d0f985e7bc4ef0643ebed471274
jedi-narwhal <soniasnlep@yahoo.com>
2026-03-16 13:17:36 -0700

    applied overrides to door and its prefabs also put all the serialzied fields for the audio manager

--- GDIM32Final/Assets/Scripts/audio manager.cs
+++ GDIM32Final/Assets/Scripts/audio manager.cs
@@ -8,15 +8,14 @@ public class audiomanager : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private AudioSource _clicksound;
     [SerializeField] private AudioSource _endsound;
-    [SerializeField] private AudioSource _audioSource;
+  
     [SerializeField] private player _player;
 
 
     void Start()
     {
         player.Instance.Click+= HandleClick;
-        player.Instance.EndGame+= HandleEndgame;// hook when the thing is done most likely going to be in ui or timer
-        _audioSource=GetComponent<AudioSource>(); 
+        player.Instance.EndGame+= HandleEndgame;// hook when the thing is done most likely going to be in ui or timer 
     }
 
     // Update is called once per frame
